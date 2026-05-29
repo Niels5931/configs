be627ddfc0137f0c5a740a8b7713386d77700591
Niels Hartmann <nwhartmann@gmail.com>
2026-05-29 20:41:28 +0200

    Added tmux config

---
+++ tmux/window-color.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+case "$1" in
+    1) echo "#bf616a";;
+    2) echo "#d08770";;
+    3) echo "#ebcb8b";;
+    4) echo "#a3be8c";;
+    5) echo "#88c0d0";;
+    6) echo "#81a1c1";;
+    7) echo "#b48ead";;
+    *) echo "#5e81ac";;
+esac
